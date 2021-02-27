7b5ec33bd993376feb635a02b25b2a284761a0a0
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-02-27 16:19:03 -0300

    typo do path para ativar o env

--- install.py
+++ install.py
@@ -3,7 +3,7 @@ from sys import platform
 
 try:
     if platform == "linux":
-        os.system("source ./env/Scrips/activate")
+        os.system("source ./env/Scripts/activate")
     elif "win" in platform:]
         os.system("./env/Scrips/activate.bat")
 except:
