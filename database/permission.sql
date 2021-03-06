1262a67d71ad59b705db0ee14d835bc64899bd6b
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-03-06 18:32:07 -0300

    correcao do grant options

--- database/permission.sql
+++ database/permission.sql
@@ -1,7 +1,7 @@
 CREATE USER 'petmall_app'@'localhost' IDENTIFIED BY 'petmall_pass';
 
-GRANT CREATE, ALTER, INSERT, UPDATE, DELETE, SELECT  on *.* TO 'sammy'@'localhost' WITH GRANT OPTION;
-
+GRANT CREATE, ALTER, INSERT, UPDATE, DELETE, SELECT  on *.* TO 'petmall_app'@'localhost' WITH GRANT OPTION;
+:
 FLUSH PRIVILEGES;
 
 CREATE DATABASE petmall_db;
