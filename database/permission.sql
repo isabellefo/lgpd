25495b53b3e6e706e7b9dec2208aec41b1d057ea
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-03-06 17:08:00 -0300

    correcao da criacao do banco

--- database/permission.sql
+++ database/permission.sql
@@ -2,7 +2,6 @@ CREATE USER 'petmall_app'@'localhost' IDENTIFIED BY 'petmall_pass';
 
 GRANT CREATE, ALTER, INSERT, UPDATE, DELETE, SELECT  on *.* TO 'sammy'@'localhost' WITH GRANT OPTION;
 
-CREATE petmall_db;
-
 FLUSH PRIVILEGES;
 
+CREATE DATABASE petmall_db;
