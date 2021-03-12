4c536f9a7a72737a58dd39a44a6e1c76f3ea0875
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-03-12 09:21:40 -0300

    re modelando unidade no app

--- petshop/unidade/Unidade.py
+++ petshop/unidade/Unidade.py
@@ -4,11 +4,16 @@ from sqlalchemy import Column, Integer, String
 
 class Unidade(Base):
     __tablename__ = "unidades"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id_unidade = Column(Integer, primary_key=True, autoincrement=True)
     razao_social = Column(String(100))
     nome_fantasia = Column(String(100))
-    cnpj = Column(String(14))
-    endereco = Column(String(500))
-    telefone = Column(String(10))
-    celular = Column(String(11))
+    cnpj = Column(String(20))
+    telefone = Column(String(20))
+    celular = Column(String(20))
     email = Column(String(100))
+    bairro = Column(String(30))
+    rua = Column(String(30))
+    numero = Column(String(10))
+    estado = Column(String(2))
+    cep = Column('CEP', String(20))
+    cidade = Column(String(20))
