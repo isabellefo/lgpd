79bd5c38f1c31b718ca2998b19760918ed550d3e
Gabriel Mendes <gabriel.mendespng@gmail.com>
2021-03-15 15:23:16 -0300

    cria scripts para popular tabela produto e produtos_unidades e +
    remove install.py
    atualiza old_schema.sql (coluna tipo em produtos_unidades alterada para ENUM)
    adiciona atributo valor na classe Produto

--- petshop/produto/Produto.py
+++ petshop/produto/Produto.py
@@ -1,8 +1,7 @@
 from petshop.database import *
-from sqlalchemy import Column, Enum, Integer, String
+from sqlalchemy import Column, Enum, Integer, String, Numeric
 
 from .TipoProduto import TipoProduto
-
 
 class Produto(Base):
     __tablename__ = "produtos"
@@ -10,3 +9,4 @@ class Produto(Base):
     id = Column('id_produto', Integer, primary_key=True, autoincrement=True)
     nome = Column(String(100))
     tipo = Column(Enum(TipoProduto))
+    valor = Column(Numeric(6, 2))
