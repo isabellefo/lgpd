2e7a7b769206d0d28a0d812bd8c220568bea78f2
Isabelle Oliveira <foliveira.isa@gmail.com>
2021-04-07 19:04:16 -0300

    corrige nome de tabela

--- database/migrate_dados_pessoais.sql
+++ database/migrate_dados_pessoais.sql
@@ -2,7 +2,7 @@ CREATE TABLE dados_pessoais (id_dado_pessoal int NOT NULL AUTO_INCREMENT, PRIMAR
 AS (SELECT id_cliente, nome, cpf, rg, telefone, celular, data_nascimento, sexo, renda FROM clientes);
 
 ALTER TABLE dados_pessoais
-ADD FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
+ADD FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente);
 
 ALTER TABLE clientes
 DROP COLUMN nome, 
