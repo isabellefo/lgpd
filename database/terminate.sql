70a265a08ef01d7a6f578b86fe1fa81ebcbd70ae
Gabriel Mendes <gabriel.mendespng@gmail.com>
2021-04-06 23:41:09 -0300

    cria script de migração de endereço

--- database/terminate.sql
+++ database/terminate.sql
@@ -5,6 +5,7 @@ drop table if exists clientes_responsaveis;
 drop table if exists transacoes;
 drop table if exists unidades;
 drop table if exists produtos;
+drop table if exists enderecos;
 drop table if exists clientes;
 drop table if exists clientes_status;
 drop table if exists endereco;
