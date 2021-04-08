39fea73bb727aabad3e9a2358167810aad14ccfc
Gabriel Borges <gabrielfr.borges@gmail.com>
2021-04-07 22:40:07 -0300

    correcoes do teste

--- database/migrate_status.sql
+++ database/migrate_status.sql
@@ -1,30 +1,25 @@
 USE petmall_db;
 
-CREATE TABLE clientes_status
+DROP TABLE IF EXISTS clientes_status;
+
+CREATE TABLE  clientes_status
 (
    id_status INTEGER PRIMARY KEY AUTO_INCREMENT,
    descricao VARCHAR(200),
    data_cadastro DATE
 );
 
-IF NOT EXISTS(SELECT 1 FROM clientes_status where descricao = "Ativo")BEGIN
-INSERT INTO clientes_status (descricao, data_cadastro)
-VALUES ("Ativo", GETDATE())
-END
+INSERT IGNORE INTO clientes_status (id_status, descricao, data_cadastro)
+VALUES (1, "Ativo", CURDATE());
 
-IF NOT EXISTS(SELECT 1 FROM clientes_status where descricao = "Inativo")BEGIN
-INSERT INTO clientes_status (descricao, data_cadastro)
-VALUES ("Inativo", GETDATE())
-END
+INSERT IGNORE INTO clientes_status (id_status, descricao, data_cadastro)
+VALUES (2, "Inativo", CURDATE());
 
-IF NOT EXISTS(SELECT 1 FROM clientes_status where descricao = "Anonimizado")BEGIN
-INSERT INTO clientes_status (descricao, data_cadastro)
-VALUES ("Anonimizado", GETDATE())
-END
+INSERT IGNORE INTO clientes_status (id_status, descricao, data_cadastro)
+VALUES (3, "Anonimizado", CURDATE());
 
 ALTER TABLE clientes
 	add id_status int,
     add constraint foreign key(id_status) references clientes_status(id_status);
-
 
 UPDATE clientes SET id_status = 1
