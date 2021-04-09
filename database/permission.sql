80c468f6cc9b700a2a401be2a07277536aa64490
Gabriel Mendes <gabriel.mendespng@gmail.com>
2021-04-09 19:16:53 -0300

    permite que o usuário petmall_app tenha todos os privilégios

--- database/permission.sql
+++ database/permission.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE petmall_db;
+CREATE DATABASE IF NOT EXISTS petmall_db;
 
 CREATE USER 'petmall_app'@'localhost' IDENTIFIED BY 'petmall_pass';
 
-GRANT CREATE, ALTER, INSERT, UPDATE, DELETE, SELECT  on petmall_db.* TO 'petmall_app'@'localhost' WITH GRANT OPTION;
+GRANT ALL on petmall_db.* TO 'petmall_app'@'localhost' WITH GRANT OPTION;
 
 FLUSH PRIVILEGES;
